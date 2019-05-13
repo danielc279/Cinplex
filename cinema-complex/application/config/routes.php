79653608702d6b94968c5b09725decd44e86ca5b
danielc279 <43954906+danielc279@users.noreply.github.com>
2019-05-13 16:39:56 +0200

    Updated Application
    -Changed Config
    -Added log in and register views and models

--- cinema-complex/application/config/routes.php
+++ cinema-complex/application/config/routes.php
@@ -49,6 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'home';
+
+$route['register'] = 'system/register';
+$route['register/submit'] = 'system/do_register';
+
+$route['login'] = 'system/login';
+$route['login/submit'] = 'system/do_login';
+
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['translate_uri_dashes'] = FALSE;;
